86a4c69282812a23006fde0ec7aba1d62cb6c829
JohnAsare <johnasare7@gmail.com>
2018-09-08 14:07:13 -0400

    "Started writing the function. I put in the Author name, date and a doc string describing the code"

---
+++ Reminder.py
@@ -0,0 +1,19 @@
+# Author: John Asare
+# Date: Sep 8 2018
+
+# Copyright: Ownership right
+
+
+""" This code is just a script/short code to help users keep track of their use of contact lenses. The code will
+ask the user if he/she has used the contact lenses and then will remind them to throw it out when needed to"""
+
+
+def contact_lenses(x):
+
+    y = 1
+    x = int(input("Worn:? "))
+    if x == "yes":
+        y -= 1
+
+
+
